e41a55ad7545fe8e233dc632f2531488b3a328da
myjhye <t01087633799@gmail.com>
2023-10-08 16:09:04 +0900

    jpa employees entity 생성

---
+++ src/main/java/net/javaguides/ems/entity/Employee.java
@@ -0,0 +1,39 @@
+package net.javaguides.ems.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+// jpa 엔티티 클래스임을 나타냄
+@Entity
+
+// 엔티티와 데이터베이스 테이블 간의 매핑정보 지정
+@Table(name = "employees", schema = "ems")
+public class Employee {
+
+    // 엔티티의 primary key인 필드
+    @Id
+    // primary key 값 자동 생성
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    // 엔티티 필드와 데이터베이스 컬럼 간의 매핑 정보 지정
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id", nullable = false, unique = true)
+    private String email;
+}
