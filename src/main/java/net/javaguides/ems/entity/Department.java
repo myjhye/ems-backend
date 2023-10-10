3fc6bd1e55e4680e5f5bb5e48d8b5e2b01d7f256
myjhye <t01087633799@gmail.com>
2023-10-10 20:03:02 +0900

    부서 테이블, 부서 엔티티, 부서 레포지토리 생성

---
+++ src/main/java/net/javaguides/ems/entity/Department.java
@@ -0,0 +1,27 @@
+package net.javaguides.ems.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "departments", schema = "ems")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "department_name")
+    private String departmentName;
+
+    @Column(name = "department_description")
+    private String departmentDescription;
+}
