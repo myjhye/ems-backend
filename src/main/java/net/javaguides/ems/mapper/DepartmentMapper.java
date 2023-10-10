21e780e9fda1345ead2bd923fc738b278f9a8e45
myjhye <t01087633799@gmail.com>
2023-10-10 23:32:59 +0900

    부서 dto, 부서 mapper 생성

---
+++ src/main/java/net/javaguides/ems/mapper/DepartmentMapper.java
@@ -0,0 +1,27 @@
+package net.javaguides.ems.mapper;
+
+import net.javaguides.ems.dto.DepartmentDto;
+import net.javaguides.ems.entity.Department;
+
+public class DepartmentMapper {
+
+    // entity -> dto 변환
+    public static DepartmentDto mapToDepartmentDto(Department department) {
+
+        return new DepartmentDto(
+                department.getId(),
+                department.getDepartmentName(),
+                department.getDepartmentDescription()
+        );
+    }
+
+    // dto -> entity 변환
+    public static Department mapToDepartment(DepartmentDto departmentDto) {
+
+        return new Department(
+                departmentDto.getId(),
+                departmentDto.getDepartmentDescription(),
+                departmentDto.getDepartmentName()
+        );
+    }
+}
