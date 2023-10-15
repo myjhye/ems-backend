fa34e56069fff363bff5f196143d232fb75ceb52
myjhye <t01087633799@gmail.com>
2023-10-15 17:54:29 +0900

    serviceImpl 구현 방식 변경 -> Modelmapper 사용

--- src/main/java/net/javaguides/ems/service/impl/TodoServiceImpl.java
+++ src/main/java/net/javaguides/ems/service/impl/TodoServiceImpl.java
@@ -5,6 +5,7 @@ import net.javaguides.ems.dto.TodoDto;
 import net.javaguides.ems.entity.Todo;
 import net.javaguides.ems.repository.TodoRepository;
 import net.javaguides.ems.service.TodoService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,25 +13,21 @@ import org.springframework.stereotype.Service;
 public class TodoServiceImpl implements TodoService {
 
     private TodoRepository todoRepository;
+    private ModelMapper modelMapper;
 
     @Override
     public TodoDto addTodo(TodoDto todoDto) {
 
         // dto -> entity 변환
-        Todo todo = new Todo();
-        todo.setTitle(todoDto.getTitle());
-        todo.setDescription(todoDto.getDescription());
-        todo.setCompleted(todoDto.isCompleted());
+        Todo todo= modelMapper.map(todoDto, Todo.class);
 
+
+        // todo jpa entity
         Todo savedTodo = todoRepository.save(todo);
 
 
         // entity -> dto 변환
-        TodoDto savedTodoDto = new TodoDto();
-        savedTodoDto.setId(savedTodo.getId());
-        savedTodoDto.setTitle(savedTodo.getTitle());
-        savedTodoDto.setDescription(savedTodo.getDescription());
-        savedTodoDto.setCompleted(savedTodo.isCompleted());
+        TodoDto savedTodoDto = modelMapper.map(savedTodo, TodoDto.class);
 
         return savedTodoDto;
     }
