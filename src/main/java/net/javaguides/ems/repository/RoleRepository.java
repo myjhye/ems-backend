4911eee20550883fc9823d50928d9a7066181ddf
myjhye <t01087633799@gmail.com>
2023-10-20 23:31:11 +0900

    role 레포지토리, user 레포지토리 생성

---
+++ src/main/java/net/javaguides/ems/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package net.javaguides.ems.repository;
+
+import net.javaguides.ems.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
