036acac64b6a62ee487685efe78a9974ffdf25ab
myjhye <t01087633799@gmail.com>
2023-10-23 22:08:54 +0900

    JWT 토큰 생성 및 검증 기능 추가

---
+++ src/main/java/net/javaguides/ems/security/JtwTokenProvider.java
@@ -0,0 +1,82 @@
+package net.javaguides.ems.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JtwTokenProvider {
+
+    @Value("${app.jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${app.jwt-expiration-milliseconds}")
+    private String jwtExpirationDate;
+
+
+
+    // jwt token 생성
+    public String generateToken(Authentication authentication) {
+
+        String username = authentication.getName();
+
+        Date currentDate = new Date();
+
+        Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate);
+
+        String token = Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .signWith(key())
+                .compact();
+
+        return token;
+    }
+
+    private Key key() {
+
+        return Keys.hmacShaKeyFor(
+                Decoders.BASE64.decode(jwtSecret)
+        );
+    }
+
+
+
+    // jwt token에서 username(유저이름) 가져오기
+    public String getUsername(String token) {
+
+        Claims claims = Jwts.parser()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+
+        String username = claims.getSubject();
+
+        return username;
+    }
+
+
+
+
+    // validate jwt token
+    public boolean validateToken(String token) {
+
+        Jwts.parser()
+                .setSigningKey(key())
+                .build()
+                .parse(token);
+
+        return true;
+    }
+
+
+}
